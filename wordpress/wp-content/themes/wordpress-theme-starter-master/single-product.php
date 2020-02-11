862d48b3f47df18a039403b38fc6dfa72e5df6df
Cruz Ramirez <cruz_ramirez@ymail.com>
2020-02-10 16:58:30 -0800

    acf single product dropdown

--- wordpress/wp-content/themes/wordpress-theme-starter-master/single-product.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/single-product.php
@@ -15,46 +15,53 @@ $Affillate_external = get_field('affiliate_external');
 $External_URL = get_field('external_link');
 
 get_header(); ?>
-	<main>
-		<div class="title"><h1><?php the_title(); ?></h1></div>
-		<?php if( $Product_Image ): ?>
-			<div class="product_img">
-				<img src="<?php echo $Product_Image['url']; ?>" alt="<?php echo $Product_Image['alt']; ?>">
-			</div>
-		<?php endif; ?>
-		<?php if( $Description ): ?>
-			<div class="description">
-				<p><?php echo $Description; ?></p>
-			</div>
-		<?php endif; ?>
-	</main>
-	<aside>
-		<?php if( $Price ): ?>
-			<div class="price">
-				<h3>Price: $<?php echo $Price; ?></h3>
-			</div>
-		<?php endif; ?>
-		<?php if( $On_sale == 1 & $Sale_price != 0): ?>
-			<div class="on_sale">
-				<h3> On Sale for $<?php echo $Sale_price;?></h3>
-			</div>
-		<?php endif; ?>
-		<?php if( $Quantity <= 10 ): ?>
-			<div class="quantity">
-				<h3> Hurry! They're <?php echo $Quantity;?> left!</h3>
-			</div>
-		<?php endif; ?>
-		<?php if( $Shirt_size ): ?>
-			<div class="shirt_size">
-				<select id="shirt_sizes">
-					<option value="volvo">Volvo</option>
-				</select>
-			</div>
-		<?php endif; ?>
-		<a href="" role="button" class="btn buy_now">Buy Now</a>
-		<a href="" role="button" class="btn buy_now">Go to Product</a>
-	</aside>
+	<div class="container">
+		<main>
+			<div class="title"><h1 class="text-center"><?php the_title(); ?></h1></div>
+			<?php if( $Product_Image ): ?>
+				<div class="product_img">
+					<img src="<?php echo $Product_Image['url']; ?>" alt="<?php echo $Product_Image['alt']; ?>">
+				</div>
+			<?php endif; ?>
+			<?php if( $Description ): ?>
+				<div class="description">
+					<p><?php echo $Description; ?></p>
+				</div>
+			<?php endif; ?>
+		</main>
+		<aside>
+			<?php if( $Price ): ?>
+				<div class="price">
+					<h3>Price: $<?php echo $Price; ?></h3>
+				</div>
+			<?php endif; ?>
+			<?php if( $On_sale == 1 && $Sale_price != 0): ?>
+				<div class="on_sale">
+					<h3> On Sale for $<?php echo $Sale_price;?></h3>
+				</div>
+			<?php endif; ?>
+			<?php if( $Quantity < 10 ): ?>
+				<div class="quantity">
+					<h3> Hurry! Only <strong style="color: red; font-weight: 700;"><?php echo $Quantity;?></strong> left!</h3>
+				</div>
+			<?php endif; ?>
+			<?php
+				if($Shirt_size)
+				{
+					echo '<select>';
+					foreach($Shirt_size as $value) {
+						foreach($value as $value) {
+							echo '<option>' . $value. '</option>';
+						}
+					}
+					echo '</select>';
+				}
+			?>
 
+			<a href="" role="button" class="btn buy_now">Buy Now</a>
+
+		</aside>
+	</div>
 
 
 
